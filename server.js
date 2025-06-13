705ea0c4db2903b5f81ebeb1a32bb111e4ea962f
andreghenssler2 <113136868+andreghenssler2@users.noreply.github.com>
2025-06-12 21:24:33 -0300

    Update server.js

--- server.js
+++ server.js
@@ -1,6 +1,6 @@
 const express = require('express');
 const app = express();
-const PORT = 6664;
+const PORT = process.env.PORT || 6664;
 
 const swaggerUi = require('swagger-ui-express');
 const YAML = require('yamljs');
